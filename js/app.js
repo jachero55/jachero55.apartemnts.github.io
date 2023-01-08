7f81c28381b8959970dfe609e1739e88e5fbd7d8
John Achero <jkachero@student.fullsail.edu>
2023-01-08 11:28:07 -0600

    Created app js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+class App {
+  constructor() {
+    this.slider = new Slider()
+  }
+
+  // Implement singleton
+  // condition to check if controller instance already created
+  // If there's no instance, we create one
+  static getInstance = () => {
+    if(!App._instance) {
+      App._instance = new App();
+      return App._instance;
+    } else {
+      // Throw an error if one already exits
+      throw "There can only be one instance of an APP!!!";
+    }
+  }
+}
+
+// self executing method
+(() => {
+  const app = App.getInstance();
+})();
